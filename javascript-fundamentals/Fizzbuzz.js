3882a06ef8e5ae434f5b1ea3602f552a23b46ce9
samir <s-malik@live.co.uk>
2022-01-24 16:11:33 +0000

    FizzBuzz Complete

---
+++ javascript-fundamentals/Fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(number) {
+    if (number % 5 === 0 && number % 3 === 0) {
+        return 'FizzBuzz';
+    }   else if (number % 5 === 0) {
+        return 'Buzz';
+    }   else if (number % 3 === 0)  {
+        return 'Fizz';
+    }   else {
+        return number;
+    }
+}
+
+for (let i = 1; i < 51; i++) {
+ console.log(`Fizzbuzz result for ${i} is ` + fizzbuzz(i) )
+}
